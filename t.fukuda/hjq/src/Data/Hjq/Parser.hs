a088518fe6b3f8ff26e2f4dc94e6a731e664dbc4
tfukudan <t.fukuda@sansan.com>
2019-04-03 16:26:59 +0900

    refactored: format

--- t.fukuda/hjq/src/Data/Hjq/Parser.hs
+++ t.fukuda/hjq/src/Data/Hjq/Parser.hs
@@ -1,36 +1,42 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Data.Hjq.Parser where
 
-import Control.Applicative ((<|>))
-import Data.Attoparsec.Text
-import Data.Text (Text, pack)
+module Data.Hjq.Parser
+  ( JqFilter(..)
+  , parseJqFilter
+  ) where
+
+import           Control.Applicative  ((<|>))
+import           Data.Attoparsec.Text
+import           Data.Text            (Text, pack)
 
 data JqFilter
-    = JqField Text JqFilter
-    | JqIndex Int JqFilter
-    | JqNil
-    deriving (Show, Read, Eq)
+  = JqField Text
+            JqFilter
+  | JqIndex Int
+            JqFilter
+  | JqNil
+  deriving (Show, Read, Eq)
 
 parseJqFilter :: Text -> Either Text JqFilter
-parseJqFilter s = showParseResult $ parse (jqFilterParser <* endOfInput) s `feed` ""
+parseJqFilter s =
+  showParseResult $ parse (jqFilterParser <* endOfInput) s `feed` ""
 
 jqFilterParser :: Parser JqFilter
 jqFilterParser = schar '.' >> (jqField <|> jqIndex <|> pure JqNil)
-    where
-        jqFilter :: Parser JqFilter
-        jqFilter = (schar '.' >> jqField) <|> jqIndex <|> pure JqNil
-        jqField :: Parser JqFilter
-        jqField = JqField <$> (word <* skipSpace) <*> jqFilter
-        jqIndex :: Parser JqFilter
-        jqIndex = JqIndex <$> (schar '[' *> decimal <* schar ']') <*> jqFilter
+  where
+    jqFilter :: Parser JqFilter
+    jqFilter = (schar '.' >> jqField) <|> jqIndex <|> pure JqNil
+    jqField :: Parser JqFilter
+    jqField = JqField <$> (word <* skipSpace) <*> jqFilter
+    jqIndex :: Parser JqFilter
+    jqIndex = JqIndex <$> (schar '[' *> decimal <* schar ']') <*> jqFilter
 
 showParseResult :: Show a => Result a -> Either Text a
 showParseResult (Done _ r) = Right r
-showParseResult r = Left . pack $ show r
+showParseResult r          = Left . pack $ show r
 
 schar :: Char -> Parser Char
 schar c = skipSpace *> char c <* skipSpace
 
 word :: Parser Text
 word = fmap pack $ many1 (letter <|> char '-' <|> char '_' <|> digit)
-
--- t.fukuda/hjq/test/Spec.hs
+++ t.fukuda/hjq/test/Spec.hs
@@ -1,19 +1,23 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Test.HUnit
-import Data.Hjq.Parser
+
+import           Data.Hjq.Parser (JqFilter (..), parseJqFilter)
+import           Test.HUnit
 
 main :: IO ()
 main = do
-    runTestTT $ TestList
-        [ jqFilterParserTest
-        ]
-    return ()
+  runTestTT $ TestList [jqFilterParserTest]
+  return ()
 
 jqFilterParserTest :: Test
-jqFilterParserTest = TestList
+jqFilterParserTest =
+  TestList
     [ "jqFilterParser test 1" ~: parseJqFilter "." ~?= Right JqNil
-    , "jqFilterParser test 2" ~: parseJqFilter ".[0]" ~?= Right (JqIndex 0 JqNil)
-    , "jqFilterParser test 3" ~: parseJqFilter ".fieldName" ~?= Right (JqField "fieldName" JqNil)
-    , "jqFilterParser test 4" ~: parseJqFilter ".[0].fieldName" ~?= Right (JqIndex 0 (JqField "fieldName" JqNil))
-    , "jqFilterParser test 5" ~: parseJqFilter ".fieldName[0]" ~?= Right (JqField "fieldName" (JqIndex 0 JqNil))
+    , "jqFilterParser test 2" ~: parseJqFilter ".[0]" ~?=
+      Right (JqIndex 0 JqNil)
+    , "jqFilterParser test 3" ~: parseJqFilter ".fieldName" ~?=
+      Right (JqField "fieldName" JqNil)
+    , "jqFilterParser test 4" ~: parseJqFilter ".[0].fieldName" ~?=
+      Right (JqIndex 0 (JqField "fieldName" JqNil))
+    , "jqFilterParser test 5" ~: parseJqFilter ".fieldName[0]" ~?=
+      Right (JqField "fieldName" (JqIndex 0 JqNil))
     ]
