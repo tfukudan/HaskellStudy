f4ecda3049a064639c1832839e3cb2b3af50f284
tfukudan <t.fukuda@sansan.com>
2019-04-03 13:42:56 +0900

    20190403回分進捗報告

--- t.fukuda/hjq/test/Spec.hs
+++ t.fukuda/hjq/test/Spec.hs
@@ -1,6 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
 import Test.HUnit
+import Data.Hjq.Parser
 
 main :: IO ()
 main = do
-    runTestTT $ "Test1" ~: 1 + 1 ~?=2
+    runTestTT $ TestList
+        [ jqFilterParserTest
+        ]
     return ()
+
+jqFilterParserTest :: Test
+jqFilterParserTest = TestList
+    [ "jqFilterParser test 1" ~: parseJqFilter "." ~?= Right JqNil
+    , "jqFilterParser test 2" ~: parseJqFilter ".[0]" ~?= Right (JqIndex 0 JqNil)
+    , "jqFilterParser test 3" ~: parseJqFilter ".fieldName" ~?= Right (JqField "fieldName" JqNil)
+    , "jqFilterParser test 4" ~: parseJqFilter ".[0].fieldName" ~?= Right (JqIndex 0 (JqField "fieldName" JqNil))
+    , "jqFilterParser test 5" ~: parseJqFilter ".fieldName[0]" ~?= Right (JqField "fieldName" (JqIndex 0 JqNil))
+    ]
