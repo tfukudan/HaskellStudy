7cce2464e3793ff97b9ad420004b642e754e69b7
tfukudan <t.fukuda@sansan.com>
2019-05-15 13:00:59 +0900

    20190515進捗分

--- t.fukuda/hjq/src/Data/Hjq/Query.hs
+++ t.fukuda/hjq/src/Data/Hjq/Query.hs
@@ -1,1 +1,33 @@
-module Data.Hjq.Query where
+{-# LANGUAGE OverloadedStrings #-}
+
+module Data.Hjq.Query
+  ( applyFilter
+  ) where
+
+import           Control.Lens        ((^?))
+import           Control.Monad       (join)
+import           Data.Aeson          (Value (..))
+import           Data.Aeson.Lens     (key, nth)
+import qualified Data.HashMap.Strict as H
+import           Data.Hjq.Parser     (JqFilter (..))
+import qualified Data.Text           as T (Text, pack)
+import qualified Data.Vector         as V
+
+applyFilter :: JqFilter -> Value -> Either T.Text Value
+applyFilter (JqField fieldName n) obj@(Object _) =
+  join $ noteNotFoundError fieldName (fmap (applyFilter n) (obj ^? key fieldName))
+applyFilter (JqIndex index n) array@(Array _) =
+  join $ noteOutOfRangeError index (fmap (applyFilter n) (array ^? nth index))
+applyFilter JqNil v = Right v
+applyFilter f o = Left $ "unexpected pattern : " <> tshow f <> " : " <> tshow o
+
+noteNotFoundError :: T.Text -> Maybe a -> Either T.Text a
+noteNotFoundError _ (Just x) = Right x
+noteNotFoundError s Nothing  = Left $ "field name not found" <> s
+
+noteOutOfRangeError :: Int -> Maybe a -> Either T.Text a
+noteOutOfRangeError _ (Just x) = Right x
+noteOutOfRangeError s Nothing  = Left $ "out of range : " <> tshow s
+
+tshow :: Show a => a -> T.Text
+tshow = T.pack . show
--- t.fukuda/hjq/test/Spec.hs
+++ t.fukuda/hjq/test/Spec.hs
@@ -1,11 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import           Data.Hjq.Parser (JqFilter (..), parseJqFilter, JqQuery(..), parseJqQuery)
+import           Control.Lens        ((^?))
+import           Data.Aeson          (Value (..))
+import           Data.Aeson.Lens     (key, nth)
+import qualified Data.HashMap.Strict as H
+import           Data.Hjq.Parser     (JqFilter (..), JqQuery (..),
+                                      parseJqFilter, parseJqQuery)
+import           Data.Hjq.Query      (applyFilter)
+import           Data.Text           (Text, unpack)
+import qualified Data.Vector         as V
 import           Test.HUnit
 
 main :: IO ()
 main = do
-  runTestTT $ TestList [jqFilterParserTest, jqQueryParserTest, jqQueryParserSpacesTest]
+  runTestTT $ TestList [jqFilterParserTest, jqQueryParserTest, jqQueryParserSpacesTest, applyFilterTest]
   return ()
 
 jqFilterParserTest :: Test
@@ -40,4 +48,52 @@ jqQueryParserSpacesTest = TestList
     parseJqQuery " [ . hoge , . piyo ] " ~?= Right (JqQueryArray [JqQueryFilter (JqField "hoge" JqNil), JqQueryFilter (JqField "piyo" JqNil)])
   , "jqQueryParser spaces test 3" ~:
     parseJqQuery "{ \"hoge\" : [ ] , \"piyo\" :  [ ] } " ~?= Right (JqQueryObject [("hoge", JqQueryArray []), ("piyo", JqQueryArray [])])
+  ]
+
+unsafeParseFilter :: Text -> JqFilter
+unsafeParseFilter t = case parseJqFilter t of
+  Right f -> f
+  Left s -> error $ "PARSE FAILURE IN A TEST : " ++ unpack s
+
+applyFilterTest :: Test
+applyFilterTest = TestList
+  [ "applyFilter test 1" ~:
+      applyFilter (unsafeParseFilter ".") testData
+      ~?= Right testData
+  , "applyFilter test 2" ~:
+      (Just $ applyFilter (unsafeParseFilter ".string-field") testData)
+      ~?= fmap Right (testData ^? key "string-field")
+  , "applyFilter test 3" ~:
+      (Just $ applyFilter (unsafeParseFilter ".nested-field.inner-string") testData)
+      ~?= fmap Right (testData ^? key "nested-field" . key "inner-string")
+  , "applyFilter test 4" ~:
+      (Just $ applyFilter (unsafeParseFilter ".nested-field.inner-number") testData)
+      ~?= fmap Right (testData ^? key "nested-field" . key "inner-number")
+  , "applyFilter test 5" ~:
+      (Just $ applyFilter (unsafeParseFilter ".array-field[0]") testData)
+      ~?= fmap Right (testData ^? key "array-field" . nth 0)
+  , "applyFilter test 6" ~:
+      (Just $ applyFilter (unsafeParseFilter ".array-field[1]") testData)
+      ~?= fmap Right (testData ^? key "array-field" . nth 1)
+  , "applyFilter test 7" ~:
+      (Just $ applyFilter (unsafeParseFilter ".array-field[2].object-in-array") testData)
+      ~?= fmap Right (testData ^? key "array-field" . nth 2 . key "object-in-array")
+  ]
+  
+testData :: Value
+testData = Object $ H.fromList
+  [ ("string-field", String "string value")
+  , ("nested-field", Object $ H.fromList
+      [ ("inner-string", String "inner value")
+      , ("inner-number", Number 100)
+      ]
+    )
+  , ("array-field", Array $ V.fromList
+      [ String "first field"
+      , String "next field"
+      , Object (H.fromList
+        [ ("object-in-array", String "string value in object-in-array")
+        ] )
+      ]
+    )
   ]
