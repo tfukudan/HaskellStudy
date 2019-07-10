e50ac3853db52a835541186639c1119f690d0bd3
tfukudan <t.fukuda@sansan.com>
2019-07-10 12:44:54 +0900

    9.4.4 処理のまとめとI/O のhjq関数追加

--- t.fukuda/hjq/src/Data/Hjq.hs
+++ t.fukuda/hjq/src/Data/Hjq.hs
@@ -1,1 +1,16 @@
-module Data.Hjq where
+{-# LANGUAGE OverloadedStrings #-}
+module Data.Hjq ( hjq ) where
+
+import Control.Error.Util (note)
+import Data.Aeson (decode)
+import Data.Aeson.Encode.Pretty (encodePretty)
+import Data.ByteString.Lazy as B
+import Data.Hjq.Parser (parseJqQuery)
+import Data.Hjq.Query (executeQuery)
+import Data.Text as T
+
+hjq :: ByteString -> T.Text -> Either T.Text ByteString
+hjq jsonString queryString = do
+    value <- note "invalid json format." $ decode jsonString
+    query <- parseJqQuery queryString
+    executeQuery query value >>= return . encodePretty
