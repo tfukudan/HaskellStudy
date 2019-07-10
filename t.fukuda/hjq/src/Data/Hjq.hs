8e58a17915b6a876c39efdd77568e9efd614c933
tfukudan <t.fukuda@sansan.com>
2019-07-10 12:50:36 +0900

    format

--- t.fukuda/hjq/src/Data/Hjq.hs
+++ t.fukuda/hjq/src/Data/Hjq.hs
@@ -1,16 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Data.Hjq ( hjq ) where
 
-import Control.Error.Util (note)
-import Data.Aeson (decode)
-import Data.Aeson.Encode.Pretty (encodePretty)
-import qualified Data.ByteString.Lazy as B
-import Data.Hjq.Parser (parseJqQuery)
-import Data.Hjq.Query (executeQuery)
-import qualified Data.Text as T
+module Data.Hjq
+  ( hjq
+  ) where
+
+import           Control.Error.Util       (note)
+import           Data.Aeson               (decode)
+import           Data.Aeson.Encode.Pretty (encodePretty)
+import qualified Data.ByteString.Lazy     as B
+import           Data.Hjq.Parser          (parseJqQuery)
+import           Data.Hjq.Query           (executeQuery)
+import qualified Data.Text                as T
 
 hjq :: B.ByteString -> T.Text -> Either T.Text B.ByteString
 hjq jsonString queryString = do
-    value <- note "invalid json format." $ decode jsonString
-    query <- parseJqQuery queryString
-    executeQuery query value >>= return . encodePretty
+  value <- note "invalid json format." $ decode jsonString
+  query <- parseJqQuery queryString
+  executeQuery query value >>= return . encodePretty
